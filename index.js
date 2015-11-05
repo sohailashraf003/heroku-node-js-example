7a25052728ba5a6b0d53d80cf1a1101042fa6f1d
Rich Leland <rich.leland@messagesystems.com>
2015-11-04 21:44:17 -0500

    Add POST endpoint to send a message

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , app = express()
+  , bodyParser = require('body-parser')
+  , SparkPost = require('sparkpost')
+  , sp = new SparkPost(process.env.SPARKPOST_API_KEY);
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// routes
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/send', function(request, response) {
+  var recipients = request.body.recipients;
+
+  recipients.forEach(function(val, index) {
+    recipients[index] = { address: val };
+  });
+
+  sp.transmissions.send({
+    transmissionBody: {
+      content: {
+        from: 'heroku-node-js-example@sparkpostbox.com',
+        subject: 'Heroku Node.js Example',
+        html:'<html><body><p>This is a successfully sent message using the Heroku Node.js example app.</p></body></html>'
+      },
+      recipients: recipients
+    }
+  }, function(err, apiResponse) {
+    if(err) {
+      response.json(err);
+    } else {
+      response.json(apiResponse.body);
+    }
+  });
+});
+
+// start the app
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
