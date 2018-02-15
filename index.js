65358fe1e974ec1d3d7118589c3489272ee15c66
Sohail Ashraf <me.sohail31@gmail.com>
2018-02-15 16:02:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,94 @@
-var express = require('express')
-  , app = express()
-  , bodyParser = require('body-parser')
-  , SparkPost = require('sparkpost')
-  , sp = new SparkPost(process.env.SPARKPOST_API_KEY);
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-// routes
-app.get('/', function(request, response) {
-  response.render('pages/index');
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
 
-app.post('/send', function(request, response) {
-  var recipients = request.body.recipients
-    , message = request.body.message;
-
-  recipients.forEach(function(val, index) {
-    recipients[index] = { address: val };
-  });
-
-  sp.transmissions.send({
-    transmissionBody: {
-      content: {
-        from: 'heroku-node-js-example@sparkpostbox.com',
-        subject: 'Heroku Node.js Example',
-        html: '<html><body>' + message + '</body></html>'
-      },
-      options: {
-        sandbox: true
-      },
-      recipients: recipients
-    }
-  }, function(err, apiResponse) {
-    if(err) {
-      response.json(err);
+app.use((req, res, next) => {
+  if (req.query.static === 'true')   {
+      next();
+  } else {
+    //if(req.query.appid !== process.env.accessKey)
+    if(req.query.appid !== process.env.accessKey) {
+      res.send("you are not authorized to access this link");
+      console.log("you are not authorized to access this link");
+      return "you are not authorized to access this link";
+      
     } else {
-      response.json(apiResponse.body);
+      console.log("Authorized");
+      next();
+    }
+  }
+  
+ 
+});
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
     }
   });
-});
-
-// start the app
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
 });
